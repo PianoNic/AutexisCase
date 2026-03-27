f1bf1e29058c71b90321bface61fb736dcae9322
PianoNic <79938743+PianoNic@users.noreply.github.com>
2026-03-27 12:05:34 +0100

    Add generated API client and fetch spec from backend URL (#5)

---
+++ src/AutexisCase.Frontend/src/api/generated/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppConfigDto';
+export * from './CreateExampleCommand';
+export * from './ExampleDto';
+export * from './ProblemDetails';
+export * from './UserDto';
